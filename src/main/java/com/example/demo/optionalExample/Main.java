ead7c8d753946c04c76bf7014e35db03749439c4
QuantumBits1 <muhdafham8@gmail.com>
2022-06-06 22:47:01 +0800

    add examples of operations applied on Optional in optionalExample package.

--- src/main/java/com/example/demo/optionalExample/Main.java
+++ src/main/java/com/example/demo/optionalExample/Main.java
@@ -1,6 +1,7 @@
 package com.example.demo.optionalExample;
 
 import java.util.Optional;
+import java.util.function.BiFunction;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +10,19 @@ public class Main {
 
         Integer result = nestingOptionalAddOperation(val1, val2).get();
         System.out.println(result);
+
+        Optional<Integer> a = Optional.of(13);
+        Optional<Integer> b = Optional.of(42);
+        BiFunction<Integer, Integer, Integer> plus = (x, y) -> x + y;
+        BiFunction<Integer, Integer, Integer> times = (x, y) -> x * y;
+
+        Optional<Integer> addResult = compute(plus, a ,b);
+        Optional<Integer> multiplyResult = compute(times, a ,b);
+
+        System.out.println("addResult: " + addResult.get());
+        System.out.println("multiplyResult: " + multiplyResult.get());
+
+        //TODO: stream of optionals & flattening
 
         Counter counter1 = new Counter(1);
         Counter counter2 = new Counter(2);
@@ -24,6 +38,10 @@ public class Main {
                         val2.flatMap( second ->
                                 Optional.of(first + second)
                         ));
+    }
+
+    public static <A, B, R> Optional<R> compute(BiFunction<A, B, R> operation, Optional<A> oa, Optional<B> ob) {
+        return oa.flatMap(a -> ob.map(b -> operation.apply(a, b)));
     }
 
     //TODO: complete this function
